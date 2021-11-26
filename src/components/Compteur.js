121c8eb818c28750e2cf7f487674ef2d51257d16
martymcleod <chainstrument@gmail.com>
2021-11-26 21:57:52 +0100

    add restart

--- src/components/Compteur.js
+++ src/components/Compteur.js
@@ -6,12 +6,17 @@ import {useState} from "react"
 
     const [count, setCount] = useState(0)
 
+    const restart = () => {
+        setCount(0)
+    }
+
     const handleClick = () => {
         setCount(count + 1)
     }
 
     return <div>
         <button onClick={handleClick}>Click here</button>
+        <button onClick={restart}>Restart</button>
         <h1> Le compteur: {count} </h1>
     </div>
 }
